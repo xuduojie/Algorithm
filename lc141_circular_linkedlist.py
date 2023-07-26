52037e33ea0e48e4414918e00a701789cf9f9487
duojie xu <xuduojie@meshcloud.com>
2023-07-26 14:54:31 +0800

    add lc141_circular_linkedlist.py

---
+++ lc141_circular_linkedlist.py
@@ -0,0 +1,22 @@
+# 通过快慢指针法在链表中找环
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast = slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
+
+
