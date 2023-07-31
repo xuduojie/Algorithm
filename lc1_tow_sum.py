78021fa1d5031bda211403c7ca12e659626c7d14
duojie xu <xuduojie@meshcloud.com>
2023-07-31 14:36:05 +0800

    add lc1_tow_sum.py

---
+++ lc1_tow_sum.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        h = {}
+        for i in range(len(nums)):
+            if (target - nums[i]) in h:
+                return [h[target - nums[i]], i]
+            h[nums[i]] = i
+        return []
