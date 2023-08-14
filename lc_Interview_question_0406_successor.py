bc4bd7993c81866155b2a6d31f5913d037227439
duojie xu <xuduojie@meshcloud.com>
2023-08-14 11:46:19 +0800

    update lc_Interview_question_0406_successor.py

--- lc_Interview_question_0406_successor.py
+++ lc_Interview_question_0406_successor.py
@@ -8,14 +8,23 @@ class TreeNode:
 
 class Solution:
     def inorderSuccessor(self, root: TreeNode, p: TreeNode) -> TreeNode:
-        successor = None  # 用来保存后继节点
-        curr = root  # 从根节点开始搜索
+        return self.findSucc(root, p.val)
 
-        while curr is not None:
-            if curr.val > p.val:
-                successor = curr  # 如果当前节点值大于 p 的值，说明后继节点可能在左子树中，暂时保存当前节点并继续搜索左子树
-                curr = curr.left
+    def findSucc(self, root, val):
+        ans = None
+        while root:
+            if val == root.val:
+                if root.right:
+                    p = root.right
+                    while p.left:
+                        p = p.left
+                    return p
+                break
+            if root.val > val:
+                if ans is None or ans.val > root.val:
+                    ans = root
+            if val < root.val:
+                root = root.left
             else:
-                curr = curr.right  # 否则，继续搜索右子树
-
-        return successor
+                root = root.right
+        return ans
